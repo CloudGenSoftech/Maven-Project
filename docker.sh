1599202fd52f8f8583a0288c9e0587d82fc16c0e
ravi <ravindra.devops@gmail.com>
2021-09-13 19:47:39 +0530

    delted target and modified docker

--- docker.sh
+++ docker.sh
@@ -2,3 +2,6 @@ sudo docker images
 sudo docker ps
 sudo docker pull centos
 sudo docker run -itd --name server1 centos
+
+docker run -itd --name appserver -h appserver -v /appserver centos /bin/bash
+
